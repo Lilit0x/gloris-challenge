9898e29501779f51961554bc461448f04b75c46b
Abdulbaasit-Seriki <baasisek01@gmail.com>
2021-05-30 01:57:13 +0100

    fixed for new requirements

--- controllers/botReply.js
+++ controllers/botReply.js
@@ -3,35 +3,20 @@ import Reply from '../models/reply.js'
 
 export const getReply = async (req, res, next) => {
     try {
-        const { botId, message } = req.body
+        const { botId } = req.body
 
-        if(!botId || !message) {
+        if(!botId ) {
             return res.status(400).json({
                 err: {
-                    msg: `invalid credentials, botID or message is missing`
+                    msg: `invalid credentials, botID is missing`
                 }
             })
         }
 
-        const { data } = await axios.post(`https://chat.ultimate.ai/api/intents`, { botId, message }, {
-            headers: {
-                authorization: `825765d4-7f8d-4d83-bb03-9d45ac9c27c0`
-            }
+        const reply = Reply.findById(botId)
+        res.status(200).json({
+            reply: reply?.reply?.text || `Sorry, reply not found`
         })
-
-        const { name } = data.intents.reduce((prev, current) => (prev.confidence > current.confidence) ? prev : current)
-
-
-        const replies = await Reply.find()
-
-        let reply 
-        replies.forEach( r => {
-            if(r.name === name) {
-                reply = r.reply.text
-            }
-        })
-
-        res.status(200).json({reply})
 
 
     } catch(err) {
